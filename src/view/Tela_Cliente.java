7568c5854d99f3ab0817d7fe58979a40485fd6d2
Luiz Gustavo <lalmeida727.lg@gmail.com>
2023-01-21 10:42:45 -0300

    Add files via upload

---
+++ src/view/Tela_Cliente.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.*;
+
+public class Tela_Cliente {
+    private JTextField text_cnpj;
+    private JTextField text_nome_fantasia;
+    private JTextField text_inscricao_estadual;
+    private JTextField text_inicio_contrato;
+    private JTextField text_fim_contrato;
+    private JComboBox listaPedidos;
+    private JPanel Tela_Cliente;
+    private JTextField text_razao_social;
+    private JScrollBar scrollBar1;
+}
---
+++ src/view/Tela_Login.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class Tela_Login {
+    private JPanel panel1;
+    private JCheckBox desejaEntrarComoAdministradorCheckBox;
+    private JTextField textField1;
+    private JButton entrarButton;
+
+}
