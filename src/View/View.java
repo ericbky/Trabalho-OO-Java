7dc33eec89b05da95e65ebc0bad69265cbf15ad5
Luiz Gustavo <lalmeida727.lg@gmail.com>
2023-01-21 00:39:17 -0300

    Add files via upload

---
+++ src/View/View.java
@@ -0,0 +1,14 @@
+package View;
+
+import javax.swing.*;
+
+public class View {
+    private JTextField textCNPJ;
+    private JTextField textRazaoSocial;
+    private JTextField textInscricaoEstadual;
+    private JTextField TextInicioContrato;
+    private JTextField textFimContrato;
+    private JComboBox comboBox1;
+    private JPanel Tela_Cliente;
+    private JTextField hóteisKalangoTextField;
+}
