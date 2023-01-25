e5a635f0f33e35ee7c1a468e2abcf5dc7cee8481
Artur Rodrigues <91507861+ArturRSA19@users.noreply.github.com>
2023-01-24 21:32:25 -0300

    Add files via upload

---
+++ src/view/Tela_Funcionario.java
@@ -0,0 +1,31 @@
+package view;
+
+import javax.swing.*;
+
+public class Tela_Funcionario extends JFrame {
+    private JLabel tituloView;
+    private JLabel tituloNome;
+    private JLabel tituloCargo;
+    private JButton cadastroBTN;
+    private JButton listarBTN;
+    private JButton editarBTN;
+    private JButton excluirBTN;
+    private JTextField nomeFunc;
+    private JTextField cargoFunc;
+    private JPanel Tela_Funcionario;
+
+    public Tela_Funcionario () {
+        setContentPane(Tela_Funcionario);
+        setTitle("Funcionário");
+        setSize(100, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        Tela_Funcionario  telaFunc = new Tela_Funcionario();
+    }
+
+}
+
