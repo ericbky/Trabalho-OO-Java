22ad53172b56c38a934812ee74776a7ba22d2d44
Artur Rodrigues <91507861+ArturRSA19@users.noreply.github.com>
2023-01-25 22:50:11 -0300

    Add files via upload

---
+++ src/view/Tela_MostrarFuncionarioAdm.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.swing.*;
+
+public class Tela_MostrarFuncionarioAdm {
+    private JPanel Tela_MostraFuncionarioAdm;
+    private JButton voltarBtn;
+    private JLabel tituloPagina;
+    private JLabel tituloDados;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton editarFuncionárioButton;
+    private JButton excluirFuncionárioButton;
+}
