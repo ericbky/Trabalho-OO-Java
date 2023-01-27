30591c6cbd5051291688e59d3331b9b69dcb5322
Eric Silveira Gomes <es9694649@gmail.com>
2023-01-27 18:45:18 -0300

    Atualizando o projeto 27/01 18:31

---
+++ src/view/Tela_MenuAdm.java
@@ -0,0 +1,63 @@
+package view;
+
+import controller.AdministradorController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+
+public class Tela_MenuAdm extends JFrame {
+    private JTextField menuTextField;
+    private JButton administradoresButton;
+    private JButton funcionáriosButton;
+    private JButton clientesButton;
+    private JPanel Tela_MenuAdm;
+    private JButton sairButton;
+
+    public Tela_MenuAdm() {
+        setContentPane(Tela_MenuAdm);
+        setTitle("Menu");
+        setSize(400, 350);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        clientesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Tela_ListarClientes tela_adm = new Tela_ListarClientes();
+                } catch (ParseException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        administradoresButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Tela_listarAdm tela_listarAdm = new Tela_listarAdm();
+            }
+        });
+        sairButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setContentPane(Tela_MenuAdm);
+                setVisible(false);
+                AdministradorController administradorController = new AdministradorController();
+                administradorController.abrirTelaLogin();
+            }
+        });
+        funcionáriosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setContentPane(Tela_MenuAdm);
+                setVisible(false);
+                try {
+                    Tela_listarFuncAdm tela_funcionarioAdm = new Tela_listarFuncAdm();
+                } catch (ParseException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+}
---
+++ src/view/Tela_listarAdm.java
@@ -0,0 +1,65 @@
+package view;
+
+import controller.AdministradorController;
+import controller.FuncionarioController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Tela_listarAdm extends JFrame {
+    private JPanel Adms;
+    private JTextField telefone_adm1;
+    private JTextField email_adm1;
+    private JTextField nome_adm1;
+    private JTextField senha_adm1;
+    private JButton voltarButton;
+    private JTextField email_admin2;
+    private JTextField email_adm3;
+    private JTextField senha_adm2;
+    private JTextField senha_adm3;
+    private JTextField telefone_adm3;
+    private JTextField telefone_adm2;
+    private JTextField nome_adm2;
+    private JTextField nome_adm3;
+
+    public Tela_listarAdm() {
+        setContentPane(Adms);
+        setTitle("Administradores");
+        setSize(650, 450);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        voltarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setContentPane(Adms);
+                setVisible(false);
+                Tela_MenuAdm tela_adm = new Tela_MenuAdm();
+            }
+        });
+
+        AdministradorController administradorController = new AdministradorController();
+
+        String[] vAdm1, vAdm2, vAdm3;
+        vAdm1 = administradorController.buscarAdministrador1();
+        vAdm2 = administradorController.buscarAdministrador2();
+        vAdm3 = administradorController.buscarAdministrador3();
+
+        email_adm1.setText(vAdm1[0]);
+        senha_adm1.setText(vAdm1[1]);
+        telefone_adm1.setText(vAdm1[2]);
+        nome_adm1.setText(vAdm1[3]);
+
+        email_admin2.setText(vAdm2[0]);
+        senha_adm2.setText(vAdm2[1]);
+        telefone_adm2.setText(vAdm2[2]);
+        nome_adm2.setText(vAdm2[3]);
+
+        email_adm3.setText(vAdm3[0]);
+        senha_adm3.setText(vAdm3[1]);
+        telefone_adm3.setText(vAdm3[2]);
+        nome_adm3.setText(vAdm3[3]);
+    }
+
+}
